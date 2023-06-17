0346e6a3e22d76a1cc1c99f4a362e494247929fa
zxhd <zxhd863943427@outlook.com>
2023-06-17 19:30:20 +0800

    init pomodoro

--- src/index.ts
+++ src/index.ts
@@ -13,15 +13,15 @@ import * as d3 from "d3"
 import { renderTarget } from "./components/target";
 import { renderProcess } from "./components/process";
 import { renderClock } from "./components/clock";
-import { test_data } from "./testData";
-import { finished } from "stream";
+import { init_data } from "./initData";
 
 window.d3 = d3
 
 const STORAGE_NAME = "menu-config";
 const DOCK_TYPE = "dock_tab";
-let currentTarget = -1
+let currentTarget = 0
 let data;
+let currentProcess = {type:"work"}
 
 export default class PluginSample extends Plugin {
 
@@ -29,7 +29,7 @@ export default class PluginSample extends Plugin {
 
     async onload() {
         this.data[STORAGE_NAME] = {readonlyText: "Readonly"};
-        data = test_data
+        data = init_data
 
         console.log("loading plugin-sample", this.i18n);
 
@@ -59,14 +59,35 @@ export default class PluginSample extends Plugin {
         <span data-type="min" class="block__icon b3-tooltips b3-tooltips__sw" aria-label="Min ${adaptHotkey("⌘W")}"><svg><use xlink:href="#iconMin"></use></svg></span>
     </div>
     <svg id = "pomodoro_dock" viewBox="0 0 1024 1024"></svg>
-    <div class="fn__flex-1 plugin-sample__custom-dock">
-        ${this.data.text}
+    <div>
+    <div class="fn__flex-1 plugin-sample__custom-dock"
+    style="
+    display: flex;
+    justify-content: space-between;
+    margin: 10px;"
+    >
+    <button 
+        class="b3-button b3-button--outline fn__flex-center" 
+        data-type="config" 
+        id="pomodoro_start">start</button>
+    <button 
+        class="b3-button b3-button--outline fn__flex-center" 
+        data-type="config" 
+        id="pomodoro_stop">stop</button>
+    <button 
+        class="b3-button b3-button--outline fn__flex-center" 
+        data-type="config" 
+        id="pomodoro_skip">skip</button>
+    </div>
     </div>
 </div>`;
-let test_process = {type:"stop"}
+
 setMainSvg();
 renderTarget(data);
-start(test_process)
+(document.getElementById("pomodoro_start") as HTMLButtonElement).onclick = function(){start()};
+(document.getElementById("pomodoro_stop") as HTMLButtonElement).onclick = stop;
+(document.getElementById("pomodoro_skip") as HTMLButtonElement).onclick = skip;
+stop()
             },
             destroy() {
                 console.log("destroy dock:", DOCK_TYPE);
@@ -97,19 +118,41 @@ start(test_process)
 
 function updateTarget(){
     currentTarget+=1
-    if (currentTarget <= 0){
-        return
-    }
-    data[currentTarget].status = "finish"
-    
-
     if (currentTarget >= 10)
         data.push({value:1, status:"unstart"})
+    currentProcess.type = currentProcess.type === "work" ? "rest" : "work"
     renderTarget(data);
 }
 
-function start(process){
+function finishTarget(){
+    data[currentTarget].status = "finish"
+    updateTarget()
+}
+
+function skipTarget(){
+
+    data[currentTarget].status = "skip"
+    updateTarget()
+}
+
+function start(){
+    let process = currentProcess
+    console.log(currentProcess)
     setTransition(process);
     renderProcess([process])
-    renderClock([process],updateTarget)
+    renderClock([process],finishTarget)
+}
+
+function stop(){
+    let process = {type:"stop"}
+    setTransition(process);
+    renderProcess([process])
+    renderClock([process],()=>{})
+}
+
+function skip(){
+    let process = {type:"stop"}
+    setTransition(process);
+    renderProcess([process])
+    renderClock([process],skipTarget)
 }
---
+++ src/initData.ts
@@ -0,0 +1,14 @@
+import { status } from "./utils"
+
+export let init_data:Array<{value:number,status:status}> = [
+    {value:1, status:"unstart"},
+    {value:1, status:"unstart"},
+    {value:1, status:"unstart"},
+    {value:1, status:"unstart"},    
+    {value:1, status:"unstart"},
+    {value:1, status:"unstart"},
+    {value:1, status:"unstart"},
+    {value:1, status:"unstart"},
+    {value:1, status:"unstart"},
+    {value:1, status:"unstart"},
+]
