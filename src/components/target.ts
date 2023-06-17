5620fc9ff022ea65c1f73e427c9be1517f37ea12
zxhd <zxhd863943427@outlook.com>
2023-06-17 12:45:45 +0800

    clean target

--- src/components/target.ts
+++ src/components/target.ts
@@ -1,9 +1,10 @@
 import * as d3 from "d3"
 import { mainSvg } from "@/utils"
 
-export type status = "unstart"|"finish" |"skip" 
-
 const radius = (512)*0.8
+let arc = d3.arc().innerRadius(radius-10).outerRadius(radius).padAngle(0.03).cornerRadius(0.01*radius)
+let pie = d3.pie().value(d=>d.value)
+let color = d3.scaleOrdinal().domain(["unstart","finish" ,"skip"]).range(['rgba(0, 0, 0, 0.15)','#39b6eb','rgba(57,182,235,.5)'])
 
 export function renderTarget(data:any){
     console.log(mainSvg)
@@ -20,22 +21,3 @@ class Target{
     }
 }
 
-let arc = d3.arc().innerRadius(radius-10).outerRadius(radius).padAngle(0.03).cornerRadius(0.01*radius)
-let pie = d3.pie().value(d=>d.value)
-
-let test_data:Array<{value:number,status:status}> = [
-    {value:1, status:"unstart"},
-    {value:1, status:"finish"},
-    {value:1, status:"skip"},
-    {value:1, status:"unstart"},    
-    {value:1, status:"unstart"},
-    {value:1, status:"unstart"},
-    {value:1, status:"unstart"},
-    {value:1, status:"unstart"},
-    {value:1, status:"unstart"},
-    {value:1, status:"unstart"},
-]
-
-let arcData = pie(test_data)
-
-let color = d3.scaleOrdinal().domain(["unstart","finish" ,"skip"]).range(['rgba(0, 0, 0, 0.15)','#39b6eb','rgba(57,182,235,.5)'])
